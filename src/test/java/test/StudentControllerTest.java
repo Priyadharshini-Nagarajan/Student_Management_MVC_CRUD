e464df92a14de2761818216304c7ae01b9f84bf3
Priyadharshini-Nagarajan <161636455+Priyadharshini-Nagarajan@users.noreply.github.com>
2024-03-04 10:50:03 +0530

    Update StudentControllerTest.java

--- src/test/java/test/StudentControllerTest.java
+++ src/test/java/test/StudentControllerTest.java
@@ -1,1 +1,26 @@
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.ui.Model;
+import org.springframework.validation.support.BindingAwareModelMap;
+import java.util.List;
+import static org.junit.Assert.assertEquals;
 
+public class StudentControllerTest {
+
+    private StudentController studentController;
+    private Model model;
+
+    @Before
+    public void setUp() {
+        studentController = new StudentController();
+        model = new BindingAwareModelMap();
+    }
+
+    @Test
+    public void testListStudents() {
+        String viewName = studentController.listStudents(model);
+        assertEquals("list-students", viewName);
+        List<Student> students = (List<Student>) model.getAttribute("students");
+        assertEquals(0, students.size());
+    }
+}
